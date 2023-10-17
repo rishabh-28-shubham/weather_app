726c8633ecee42cd12933f4c126b7772d7f7c5a6
Rishabh Vishwakarma <ectopdash@gmail.com>
2023-10-17 13:58:26 +0530

    integ. Api_Key | Fetching weather

--- lib/Pages/Weather_Page.dart
+++ lib/Pages/Weather_Page.dart
@@ -11,14 +11,32 @@ class Weather_Page extends StatefulWidget {
 
 class _Weather_PageState extends State<Weather_Page> {
 //api key
-  final _weatherServices = Weather_Services(api_keys);
+  final _weatherServices = Weather_Services('f680d549452175ac014bb0351712aa9d');
   //a weather object
   Weather? _weather;
 
 //fetch "weather"
   _fetchWeather() async {
-    //identifing current city
-    String City_Name = await _weatherServices.Know_Current_City();
+    try {
+      // Identifying current city
+      String City_Name = await _weatherServices.Know_Current_City();
+
+      // Get weather for the identified city
+      final weather = await _weatherServices.Know_Weather(City_Name);
+
+      setState(() {
+        _weather = weather;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+//init state
+  @override
+  void initState() {
+    super.initState();
+    _fetchWeather();
   }
 
   @override
