f9cf26cd87710c79e1350283cb57bbbf8d280199
Rishabh Vishwakarma <ectopdash@gmail.com>
2023-10-17 13:01:15 +0530

    iddentifing current city

--- lib/Pages/Weather_Page.dart
+++ lib/Pages/Weather_Page.dart
@@ -10,17 +10,15 @@ class Weather_Page extends StatefulWidget {
 }
 
 class _Weather_PageState extends State<Weather_Page> {
-
-
 //api key
   final _weatherServices = Weather_Services(api_keys);
   //a weather object
   Weather? _weather;
 
-
 //fetch "weather"
-  _fetchWeather() async{
-    
+  _fetchWeather() async {
+    //identifing current city
+    String City_Name = await _weatherServices.Know_Current_City();
   }
 
   @override
