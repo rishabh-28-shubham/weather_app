f903dec4c19f5c170b13169090ea9302a49571de
Rishabh Vishwakarma <ectopdash@gmail.com>
2023-10-17 12:52:03 +0530

    resiting a Weather Model

---
+++ lib/model/weather_model.dart
@@ -0,0 +1,23 @@
+import 'dart:ffi';
+
+class Weather {
+  final String City_Name;
+  final Double Temperature;
+  final String Main_Condition;
+
+  //calling a constructor
+  Weather({
+    required this.City_Name,
+    required this.Temperature,
+    required this.Main_Condition,
+  });
+
+//a factory constructor which returns the prexisting instance by reusing it
+  factory Weather.fromJson(Map<String, dynamic> json) {
+    return Weather(
+        City_Name: json['name'],
+        Temperature: json['main']['temp'].toDouble(),
+        Main_Condition: json['weather'][0]['main']
+      );
+  }
+}
